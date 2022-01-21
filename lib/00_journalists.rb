63ba37fa16fdfbf4783756c694fbfe4a0068f906
kimialary <97555683+kimialary@users.noreply.github.com>
2022-01-21 01:10:54 +0100

    journée de galères

---
+++ lib/00_journalists.rb
@@ -0,0 +1,140 @@
+journalistes = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+$liste = journalistes.uniq
+
+#choix des questions
+def user_questions
+  puts "Salut !"
+  puts "Ça c'est une liste avec des @usernames Twitter plus longue que mon avenir."
+  puts ""
+  puts "Tu veux la réponse à quelle question?"
+  puts "Écris le numéro de la question chakal."
+  puts ""
+  puts "1. Le nombre de journalistes dans la liste."
+  puts "2. Le username de journaliste le plus court."
+  puts "3. Combien y-a-t'il d'usernames contenant 5 caractères ?(le @ ne compte pas pour un caractère)"
+  puts "4. Le nombre d'usernames commençant par une majuscule."
+  puts "5. La liste triée par ordre alphabétique."
+  puts "6. La liste triée par taille d'usernames (les plus petits en premiers, les plus grands après)."
+  puts "7. La position de @epenser dans la liste."
+  puts "8. Une répartition des usernames par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)"
+  puts ""
+  puts "Tape x pour m'abandonner.."
+  puts "Tape ta réponse :"
+  puts ">"
+  return gets.chomp
+end
+
+
+def run(input)
+ while true
+   case input
+   when '1'
+     puts qu_1
+   when '2'
+     puts qu_2
+   when '3'
+     puts qu_3
+   when '4'
+    puts qu_4
+    when '5'
+      puts qu_5
+    when "6"
+      puts qu_6
+    when "7"
+      puts qu_7
+    when '8'
+      puts qu_8
+   
+    when 'x' || 'X'
+      puts "ciao bella"
+      exit!
+    end
+    puts "Tu veux une autre question bg?"
+    puts ">"
+    input = gets.chomp
+end
+end
+
+def perform
+  run(user_questions)
+end
+
+
+# 1.
+def qu_1
+    puts "La liste contient #{$liste.length} journalistes."
+  end
+
+# 2.
+def qu_2
+    i=0
+    $liste.each do |item|
+      if item =~/[0-9]/
+        i+=1
+      end
+    end
+    "L'username le plus court est #{$liste[i]}."
+  end
+
+# 3.
+def qu_3
+    puts "Il y a #{$liste.count { |l| l.length == 6}} usernames contenant 5 caractères (sans le @)."
+end
+
+# 4. fonctionne pas
+def qu_4
+     i=0
+        $liste.each do |item|
+          if item[0..379] = /[A-Z]/
+            i+=1
+          end
+        end
+        "Cette liste contient #{i} usernames commençant par une majucsule."
+    end
+  
+  # 5.
+  def qu_5
+    print "Voici la liste triée par ordre alphabétique : #{$liste.sort}"
+    puts ""
+  end
+
+# 6.
+def qu_6
+    puts "Voici les usernames classés du plus court au plus long #{$liste.sort_by!{|x| x.size}}"
+    puts ""
+  end
+
+# 7.
+def qu_7
+    i = 1
+    $liste.each do |item|
+      if item != "@epenser"
+        i += 1
+      else
+        break
+      end
+    end
+    "La position de @epenser est #{i}"
+  end
+# 8. fonctionne pas
+def qu_8
+    i = 0
+    compteur = 0
+    $liste.each do |item| 
+      item.each_char do |character|
+        while compteur < 40
+            compteur+=1
+        end
+        if character == /[1-40]/
+        i += 1
+        end
+    end
+end
+    return "Il y a #{i} usernames qui a #{character} caractère."
+end
+
+
+
+#----------------
+perform
